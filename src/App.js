d18cdf80e9569147f76bf74f4e351749ae6364a6
ghilger16 <ghilger16@yahoo.com>
2021-02-11 18:08:27 -0700

    first commits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return <h2>Hello World</h2>;
+  return (
+    <div className="App">
+      <menu class="text-primary">Hello World</menu>
+    </div>
+  );
 }
 
 export default App;
